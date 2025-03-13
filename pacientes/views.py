b20259766c3d216d8605d03a8d4cbf962921312b
jupa <jupadema291@gmail.com>
2025-03-13 00:22:37 -0500

    c

--- pacientes/views.py
+++ pacientes/views.py
@@ -5,10 +5,10 @@ from django.http import JsonResponse
 # Create your views here.
 
 def obtener_historiasClinicas(request):
-    response = requests.get('http://localhost:8000/historias')
+    response = requests.get('http://192.168.3.54:8000/historias')
     return JsonResponse(response.json(), safe=False)
 
 def obtener_historiaClinica(request, history_id):
-    response = requests.get(f'http://localhost:8000/historia/{history_id}')
+    response = requests.get(f'http://192.168.3.54:8000/historia/{history_id}')
     return JsonResponse(response.json(), safe=False)
 
